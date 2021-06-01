a33892176de0c4c5f4e71820c9f006fa39ca525e
Min <bell8752573@yahoo.com.tw>
2021-06-01 15:30:46 +0800

    Work

---
+++ src/main/java/com/car/Work.java
@@ -0,0 +1,13 @@
+package com.car;
+
+import javax.swing.*;
+
+public class Work {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(2124, 400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
--- src/main/java/min/box/Box.java
+++ src/main/java/min/box/Box.java
@@ -1,16 +1,16 @@
-package fastbox;
+package min.box;
 
 public abstract class Box {
     int length;
     int width;
     int height;
 
-    public boolean validate(int a, int b, int c) {
-        return (a <= length && b <= width && c <= height);
+    public boolean validate(int a,int b,int c){
+        return (a <= length && b<= width && c<= height)
     }
 
     public abstract String getName();
 
     public abstract int getPrice();
-}
 
+}
